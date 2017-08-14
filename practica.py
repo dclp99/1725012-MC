afd43f91c7da890e6136ae04c59ae4514f965a9a
dclp99 <30885500+dclp99@users.noreply.github.com>
2017-08-13 20:11:19 -0500

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,23 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> num1 = 10
+>>> num2 = 3.1415
+>>> suma = num1 + num2
+>>> print (suma)
+13.1415
+>>> resta = num1-num2
+>>> print (resta)
+6.8585
+>>> num1<num2
+False
+>>> print (suma, resta)
+(13.1415, 6.858499999999999)
+>>> print( "ja"*5)
+jajajajaja
+>>> if (num1 > num2) :
+	print("hola mundo")
+
+hola mundo
+>>> print("la respuesta es: ", suma)
+('la respuesta es: ', 13.1415)
+>>> 
