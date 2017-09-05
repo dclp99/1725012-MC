436ab2d5e68bb9cee0af3a3e833312485bae11c8
dclp99 <30885500+dclp99@users.noreply.github.com>
2017-09-04 22:50:27 -0500

    Update burbuja.py
    Adición de una variable contador, para registrar el numero de iteraciones

--- burbuja.py
+++ burbuja.py
@@ -1,7 +1,12 @@
+#Algoritmo de ordenacion Bubble
+
 #declaracion de funciones
+contador=0
 def burbuja(A):
+	global contador
 	for i in range(1,len(A)):
 		for j in range(0,len(A)-1):
+			contador+=1
 			if(A[j+1]<A[j]):
 				aux=A[j]
 				A[j]=A[j+1]
@@ -12,4 +17,4 @@ def burbuja(A):
 A=[6,5,3,1,8,7,2,4]
 print(A)
 burbuja(A)
-
+print(contador)
